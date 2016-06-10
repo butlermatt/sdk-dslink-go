993073d6ff3335967c0b6823a5e1c8145a6432ea
Matthew Butler <butler.matthew@gmail.com>
2016-06-10 14:23:18 -0300

    Complete crpyto package.
    Fix bugs
    Add missing methods required by the SDK.

--- crypto/pk.go
+++ crypto/pk.go
@@ -5,10 +5,10 @@ import (
 	"crypto/sha256"
 	"encoding/base64"
 	"errors"
+	"fmt"
 	"io"
 	"math/big"
 	"strings"
-	"fmt"
 )
 
 // ECDH manages creating and providing keys.
@@ -17,18 +17,19 @@ type ECDH interface {
 	Marshal(PrivateKey) (string, error)
 	Unmarshal(string) (PrivateKey, error)
 	UnmarshalPublic(string) (PublicKey, error)
-	GenerateSharedSecret(PrivateKey, PublicKey) ([]byte, error)
+	GenerateSharedSecret(PrivateKey, PublicKey) []byte
+	HashSalt(string, []byte) string
 }
 
 // NewECDH returns a new Elliptic ECDH
 func NewECDH() ECDH {
-	return &ellipticECDH{elliptic.P256(), base64.RawURLEncoding}
+	return &ellipticECDH{Curve: elliptic.P256(), base: base64.RawURLEncoding}
 }
 
 type ellipticECDH struct {
 	ECDH
 	Curve elliptic.Curve
-	base  base64.Encoding
+	base  *base64.Encoding
 }
 
 type PublicKey struct {
@@ -40,12 +41,29 @@ func (p PublicKey) marshal() []byte {
 	return elliptic.Marshal(p.Curve, p.X, p.Y)
 }
 
+// Base64 returns a Base64 Raw Url Encoded (no padding) string of the bytes for the
+// public key.
 func (p PublicKey) Base64() string {
 	return base64.RawURLEncoding.EncodeToString(p.marshal())
 }
 
+// Hash64 returns the SHA256 check sum of the bytes for the public key encoded
+// as Base64 Raw Url encoded (no padding) string.
 func (p PublicKey) Hash64() string {
-	return base64.RawURLEncoding.EncodeToString(sha256.Sum256(p.marshal()))
+	s := sha256.Sum256(p.marshal())
+	return base64.RawURLEncoding.EncodeToString(s[:])
+}
+
+// DsId generates the dsId for this Public Key based on the prefix supplied.
+// DsId returned should be the prefix
+func (p PublicKey) DsId(prefix string) string {
+	return fmt.Sprintf("%s%s", prefix, p.Hash64())
+}
+
+// VerifyDsId confirms that the provided dsid matches the expected Hash64 for this
+// public key.
+func (p PublicKey) VerifyDsId(dsid string) bool {
+	return strings.HasSuffix(dsid, p.Hash64())
 }
 
 type PrivateKey struct {
@@ -63,7 +81,7 @@ func (e *ellipticECDH) GenerateKey(rand io.Reader) (PrivateKey, error) {
 		return priv, err
 	}
 
-	return PrivateKey{PublicKey{Curve: e.Curve, X: x, Y: y}, D: d}, nil
+	return PrivateKey{PublicKey: PublicKey{Curve: e.Curve, X: x, Y: y}, D: d}, nil
 }
 
 // Marshal converts a Private/Public key pair into a Base64.RawUrlEncoded string.
@@ -72,7 +90,7 @@ func (e *ellipticECDH) GenerateKey(rand io.Reader) (PrivateKey, error) {
 func (e *ellipticECDH) Marshal(priv PrivateKey) (string, error) {
 	pd := e.base.EncodeToString(priv.D)
 	pm := priv.PublicKey.Base64()
-	return fmt.Sprintf("%s %s", pd, pm)
+	return fmt.Sprintf("%s %s", pd, pm), nil
 }
 
 // Unmarshal will decode a Base64.RawUrlEncoded string into a Private/Public key pair.
@@ -95,11 +113,11 @@ func (e *ellipticECDH) Unmarshal(str string) (PrivateKey, error) {
 		if err != nil {
 			return priv, err
 		}
-		priv = PrivateKey{PublicKey{Curve: pub.Curve, X: pub.X, Y: pub.Y}, D: d}
+		priv = PrivateKey{PublicKey: PublicKey{Curve: pub.Curve, X: pub.X, Y: pub.Y}, D: d}
 		return priv, nil
 	case 1:
 		x, y := e.Curve.ScalarBaseMult(d)
-		priv = PrivateKey{PublicKey{Curve: e.Curve, X: x, Y: y}, D: d}
+		priv = PrivateKey{PublicKey: PublicKey{Curve: e.Curve, X: x, Y: y}, D: d}
 		return priv, nil
 	default:
 		return priv, errors.New("too many sections to unmarshal.")
@@ -124,4 +142,20 @@ func (e *ellipticECDH) UnmarshalPublic(str string) (PublicKey, error) {
 
 	pub = PublicKey{Curve: e.Curve, X: x, Y: y}
 	return pub, nil
+}
+
+// GenerateSharedSecret Creates a shared secret based on the private of one and
+// public key of the other. Returns a byte slice.
+func (e *ellipticECDH) GenerateSharedSecret(priv PrivateKey, pub PublicKey) []byte {
+
+	x, _ := e.Curve.ScalarMult(pub.X, pub.Y, priv.D)
+	return x.Bytes() // RFC5903 states we should only return X.
+}
+
+// HashSalt adds the provided string salt to the SharedSecret byte slice sec.
+// It returns a Base64 RawUrl encoded string of the SHA256 Sum of bytes.
+func (e *ellipticECDH) HashSalt(salt string, sec []byte) string {
+	raw := append([]byte(salt), sec...)
+	s := sha256.Sum256(raw)
+	return e.base.EncodeToString(s[:])
 }
